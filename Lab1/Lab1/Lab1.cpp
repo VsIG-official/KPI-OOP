95582f229e0f8207a389d722a28478725a6dac11
VsIG <Valentyn2202@gmail.com>
2020-09-02 15:15:11 +0300

    ✨: add some unworking code

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -160,18 +160,28 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         }
         break;
     case WM_PAINT:
-        {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hWnd, &ps);
+        HDC hdc_x = CreateCompatibleDC(NULL);
+        HBITMAP hBitmap = (HBITMAP)LoadImage(NULL, "wallpaper.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE); //Load the bitmap
+        SelectObject(hdc_x, hBitmap); //Put the bitmap into the hdc_x
 
-        SetTextColor(hdc, RGB(0, 0, 0)); //White text.
+        RECT rect;
+        GetWindowRect(hWnd, &rect);
 
-        TextOut(hdc, 0, 0, L"We love nice pictures.", strlen("We love nice pictures.")); //Print it!
+        BitBlt(hdc, 0, 0, rect.right - rect.left, 400, hdc_x, 0, 0, SRCCOPY); //Draw it.
+
+        HFONT hFont = CreateFont(48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Roboto Th"); //Create the font. (I'm using Roboto Thin.)
+        DeleteDC(hdc_x); //Delete the HDC containing the bitmap.
+        SelectObject(hdc, hFont);
+
+        SetTextColor(hdc, RGB(255, 255, 255)); //White text.
+        SetBkMode(hdc, TRANSPARENT); //Transparent background.
+
+        TextOut(hdc, 0, 0, "We love nice pictures.", strlen("We love nice pictures.")); //Print it!
 
         ReleaseDC(hWnd, hdc);
         EndPaint(hWnd, &ps);
-        }
-        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
--- Lab1/Lab1/module1.cpp
+++ Lab1/Lab1/module1.cpp
@@ -4,7 +4,7 @@
 
 int symbols;
 LPTSTR buffer; //LPCSTR
-HDC hdc;
+//HDC hdc;
 UINT textToShow;
 
 //Callback-функція вікна
@@ -19,7 +19,6 @@ static BOOL CALLBACK MyWork_MOD1(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM
             //зчитуємо вміст елементу Edit Control у буфер
 
             GetDlgItemText(hDlg, IDC_EDIT1, buffer, symbols);
-            TextOut(hdc, TA_CENTER, TA_CENTER, buffer, symbols);
 
             //... можливо, щось ще
             break;
@@ -28,11 +27,15 @@ static BOOL CALLBACK MyWork_MOD1(HWND hDlg, UINT iMessage, WPARAM wParam, LPARAM
         break;
     case WM_PAINT:
         PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hDlg, &ps);
 
-        // TODO: Добавьте сюда любой код прорисовки, использующий HDC...
-        //TextOutW(hdc, 10, 10, buffer, 18);
+        SetTextColor(hdc, RGB(0, 0, 0)); //White text.
 
-    default: break;
+        TextOut(hdc, 0, 0, "We love nice pictures.", strlen("We love nice pictures.")); //Print it!
+
+        ReleaseDC(hDlg, hdc);
+        EndPaint(hDlg, &ps);
+        break;
     }
     return FALSE;
 }
