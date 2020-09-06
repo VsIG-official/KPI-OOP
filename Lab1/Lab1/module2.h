3894b4642ddd9165ee32dfc5a4eb6cf743342117
VsIG <Valentyn2202@gmail.com>
2020-09-06 15:40:49 +0300

    ✨: add new dialogs

--- Lab1/Lab1/module2.h
+++ Lab1/Lab1/module2.h
@@ -1,14 +1,21 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by module2.rc
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется module2.rc
+//
+#define IDBACK_MOD2                     3
+#define IDD_DIALOG1_MOD2                101
+#define IDD_DIALOG2_MOD2                103
+#define IDNEXT_MOD2                     1001
+#define IDCANCEL2_MOD2                  1002
+#define IDOK2_MOD2                      1003
 
-// Следующие стандартные значения для новых объектов
+// Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
