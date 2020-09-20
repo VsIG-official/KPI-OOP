19dbf3a68ca3f0467302b4a9d0ab7b9976a7be87
VsIG <Valentyn2202@gmail.com>
2020-09-20 16:50:35 +0300

    Update shape.cpp

--- Lab2/Lab2/shape.cpp
+++ Lab2/Lab2/shape.cpp
@@ -9,6 +9,17 @@ protected:
 public:
 	void Set(long x1, long y1, long x2, long y2);
 	virtual void Show(HDC) = 0;
+	ShapeObjectsEditor(void);
+	~ShapeObjectsEditor();
+	void StartPointEditor();
+	void StartLineEditor();
+	void StartRectEditor();
+	void StartEllipseEditor();
+	void OnLBdown(HWND);
+	void OnLBup(HWND);
+	void OnMouseMove(HWND);
+	void OnPaint(HWND);
+	void OnInitMenuPopup(HWND, WPARAM);   //відповідно варіанту завдання
 };
 
 //---визначення методу Set---
