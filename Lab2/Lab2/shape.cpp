c0e174575ed61654ac28f58a5e13321587aa731a
VsIG <Valentyn2202@gmail.com>
2020-09-21 13:16:28 +0300

    ✨: add functions

--- Lab2/Lab2/shape.cpp
+++ Lab2/Lab2/shape.cpp
@@ -1,6 +1,8 @@
 //---Файл shape.h---
 #include "pch.h"
 
+static void StartPointEditor();
+
 //---оголошення класу---
 class Shape
 {
@@ -35,7 +37,87 @@ void Shape::Set(long x1, long y1, long x2, long y2)
 	ys2 = y2;
 }
 
-void StartPointEditor()
+void Shape::SetColor(COLORREF newColor)
 {
-
+	color = newColor;
 }
+
+void Shape::SetFill(BOOL isF)
+{
+	isFill = isF;
+}
+
+void PointShape::Show(HDC hdc)
+{
+	SetPixel(hdc, xs1, ys1, color);
+}
+
+void LineShape::Show(HDC hdc)
+{
+	HPEN hPen, hPenOld;
+
+	hPen = CreatePen(PS_SOLID, 1, color);
+	hPenOld = (HPEN)SelectObject(hdc, hPen);
+
+	MoveToEx(hdc, xs1, ys1, NULL);
+	LineTo(hdc, xs2, ys2);
+
+	SelectObject(hdc, hPenOld);
+	DeleteObject(hPen);
+}
+
+void RectShape::Show(HDC hdc)
+{
+	HPEN hPen, hPenOld;
+	HBRUSH hBrush, hBrushOld;
+
+	hPen = CreatePen(PS_SOLID, 1, color);
+	hPenOld = (HPEN)SelectObject(hdc, hPen);
+	if (isFill)
+	{
+		hBrush = CreateSolidBrush(color);
+		hBrushOld = (HBRUSH)SelectObject(hdc, hBrush);
+		SelectObject(hdc, hBrush);
+		Rectangle(hdc, xs1, ys1, xs2, ys2);
+		SelectObject(hdc, hBrushOld);
+		DeleteObject(hBrush);
+	}
+	else
+	{
+		MoveToEx(hdc, xs1, ys1, NULL);
+		LineTo(hdc, xs1, ys2);
+		LineTo(hdc, xs2, ys2);
+		LineTo(hdc, xs2, ys1);
+		LineTo(hdc, xs1, ys1);
+	}
+
+	SelectObject(hdc, hPenOld);
+	DeleteObject(hPen);
+}
+
+void EllipseShape::Show(HDC hdc)
+{
+	HPEN hPen, hPenOld;
+	HBRUSH hBrush, hBrushOld;
+
+	hPen = CreatePen(PS_SOLID, 1, color);
+	hPenOld = (HPEN)SelectObject(hdc, hPen);
+
+	if (isFill)
+	{
+		hBrush = CreateSolidBrush(color);
+		hBrushOld = (HBRUSH)SelectObject(hdc, hBrush);
+		SelectObject(hdc, hBrush);
+		Ellipse(hdc, xs1, ys1, xs2, ys2);
+		SelectObject(hdc, hBrushOld);
+		DeleteObject(hBrush);
+	}
+	else
+	{
+		Arc(hdc, xs1, ys1, xs2, ys2, 0, 0, 0, 0);
+	}
+
+	SelectObject(hdc, hPenOld);
+	DeleteObject(hPen);
+};
+
--- Lab2/Lab2/shape.h
+++ Lab2/Lab2/shape.h
@@ -2,4 +2,34 @@
 
 //---Файл shape.h---
 //оголошення класу
-Shape class Shape;
+class Shape {
+protected:
+	long xs1, ys1, xs2, ys2;
+	COLORREF color;
+	BOOL isFill;
+public:
+	void Set(long, long, long, long);
+	void SetColor(COLORREF);
+	void SetFill(BOOL);
+	virtual void Show(HDC) = 0;
+};
+
+class PointShape : public Shape
+{
+	void Show(HDC);
+};
+
+class LineShape : public Shape
+{
+	void Show(HDC);
+};
+
+class RectShape : public Shape
+{
+	void Show(HDC);
+};
+
+class EllipseShape : public Shape
+{
+	void Show(HDC);
+};
