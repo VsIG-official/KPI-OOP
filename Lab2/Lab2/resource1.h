c274610bb38a841407a46c6ef2eaeb2dec4573cf
VsIG <Valentyn2202@gmail.com>
2020-09-27 23:17:10 +0300

    Update resource1.h

--- Lab2/Lab2/resource1.h
+++ Lab2/Lab2/resource1.h
@@ -6,12 +6,5 @@
 #define IDM_ELLIPSE                     32778
 
 static COLORREF red = RGB(255, 0, 0);
-static COLORREF green = RGB(0, 255, 0);
 static COLORREF blue = RGB(0, 0, 255);
-static COLORREF lite_blue = RGB(0, 255, 255);
-static COLORREF white = RGB(255, 255, 255);
-static COLORREF yellow = RGB(255, 255, 0);
-static COLORREF pink = RGB(255, 0, 255);
-static COLORREF orange = RGB(255, 128, 0);
-static COLORREF grey = RGB(192, 192, 192);
 static COLORREF black = RGB(0, 0, 0);
