39e5e76c2d023b6979954fc73c8e8df84dceb59b
VsIG <Valentyn2202@gmail.com>
2020-10-03 16:48:56 +0300

    ✨: add bitmap

--- Lab3/Lab2/framework.h
+++ Lab3/Lab2/framework.h
@@ -3,7 +3,10 @@
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
 // Файлы заголовков Windows
+#define APSTUDIO_HIDDEN_SYMBOLS
 #include <windows.h>
+#include "prsht.h"
+#undef APSTUDIO_HIDDEN_SYMBOLS
 // Файлы заголовков среды выполнения C
 #include <stdlib.h>
 #include <malloc.h>
