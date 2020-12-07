82fe5de75859356d06d70ea02866ef55fa7a48bc
VsIG <Valentyn2202@gmail.com>
2020-12-07 22:00:19 +0200

    ✨: delete all in file, when first object is created

--- Lab5/Lab2/my_table.cpp
+++ Lab5/Lab2/my_table.cpp
@@ -4,6 +4,7 @@
 
 static string name = "objects.txt";
 static LPCSTR exceptionString = "Can't open a file or find a file";
+INT countForShapes = 0;
 
 /// <summary>
 /// Add shape to table
@@ -11,19 +12,28 @@ static LPCSTR exceptionString = "Can't open a file or find a file";
 /// <param name="shapeDetails">name and coords</param>
 void MyTable::Add(HWND hWndDlg, std::string shapeDetails)
 {	
+	
 	ofstream myTableFile;
+	if (countForShapes == 0)
+	{
+		myTableFile.open(name, std::ofstream::out | std::ofstream::trunc);
+		myTableFile.close();
+	}
+
 	myTableFile.open(name, ofstream::app);
 
 	if (myTableFile.is_open())
 	{
+
 		myTableFile << shapeDetails << endl;
 	}
 	else
 	{
 		throw new exception(exceptionString);
 	}
-
 	myTableFile.close();
+
+	countForShapes++;
 
 	SendDlgItemMessage(hWndDlg, IDC_LIST, LB_ADDSTRING,
 		0, (LPARAM)shapeDetails.c_str());
