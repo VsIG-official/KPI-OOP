2d9d578fd738f9a5a1f02339f687b650903a1af0
VsIG <Valentyn2202@gmail.com>
2020-12-08 14:15:33 +0200

    ✨: start 6-th lab

---
+++ Lab6/Lab2/framework.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+// Файлы заголовков Windows
+#define APSTUDIO_HIDDEN_SYMBOLS
+#include <windows.h>
+#include "prsht.h"
+#undef APSTUDIO_HIDDEN_SYMBOLS
+// Файлы заголовков среды выполнения C
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <fstream>
+#include <string>
+
+//--підключення бібліотеки Common Control Library--
+#include <commctrl.h>
+#pragma comment(lib, "comctl32.lib")
